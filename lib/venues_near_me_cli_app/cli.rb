c9c02587664fe5e720854026357ec2bc500323f0
Antonio Reid <read.musik@gmail.com>
2018-02-19 22:57:56 +0000

    Done.

--- lib/venues_near_me_cli_app/cli.rb
+++ lib/venues_near_me_cli_app/cli.rb
@@ -37,7 +37,7 @@ class VenuesNearMeCliApp::CLI
       input = gets.strip.downcase
       if input.to_i > 0 && @venues.length >= input.to_i
         the_venue = @venues[input.to_i - 1]
-        puts "#{the_venue.name} - #{the_venue.phone} - #{the_venue.address} - #{the_venue.url}"
+        puts "#{the_venue.name} - #{the_venue.phone} - #{the_venue.address}"
       elsif input == "list"
         list_venues
       elsif input == "exit"
